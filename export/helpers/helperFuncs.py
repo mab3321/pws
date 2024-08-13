fd4786ff77dd15dba7a5f37f309aeba8e4bc5d72
MAB <muhammadab3321@gmail.com>
2024-08-13 21:05:25 +0500

    Merged PDFs

--- export/helpers/helperFuncs.py
+++ export/helpers/helperFuncs.py
@@ -244,17 +244,6 @@ def merge_pdfs(pdf_path1, pdf_path2, output_dir=None):
     # Ensure the 'uploads_merged' directory exists
     os.makedirs(output_dir, exist_ok=True)
 
-    # Empty the 'uploads_merged' directory if it exists
-    for filename in os.listdir(output_dir):
-        file_path = os.path.join(output_dir, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print(f'Failed to delete {file_path}. Reason: {e}')
-
     # Extract the base name of the first PDF file (without extension)
     base_name = os.path.splitext(os.path.basename(pdf_path1))[0]
     
